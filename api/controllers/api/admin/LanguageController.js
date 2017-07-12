848526ce3b01b7b8de6a8013212ce1d84eedff01
hsiangtzu <a841225@gmail.com>
2017-07-12 14:57:41 +0800

    update controller的部分

---
+++ api/controllers/api/admin/LanguageController.js
@@ -0,0 +1,66 @@
+module.exports = {
+  create: async(req, res) => {
+    const data = req.body;
+    // sails.log.info('create language controller =>', data);
+    try {
+      const language = await LanguageService.create(data);
+      const checkLanguageResult = language.name === data.languageName;
+      if(checkLanguageResult) {
+        res.ok ({
+          message: 'create language success.',
+          data: language
+        });
+      }else {
+        throw new Error('Create ${data.languageName} failed.');
+      }
+    }catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  find: async(req, res) => {
+    const {name} = req.params;
+    try {
+      const language = await LanguageService.findByLanguageName(name);
+      res.ok ({
+        message: 'Get language success.',
+        data: language
+      });
+    }catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  update: async(req, res) => {
+    const {name} = req.params;
+    const data = req.body;
+    try {
+      // sails.log.info('update language controller name =>', name);
+      // sails.log.info('update language controller body =>', data);
+      const language = await LanguageService.update(data, {
+        where: {name},
+      });
+
+      res.ok ({
+        message: 'Update language success.',
+        data: language
+      })
+    }catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  destroy: async(req, res) => {
+    const {name} = req.params;
+    try {
+      // sails.log.info('delete language controller =>', name);
+      const language = await LanguageService.deleteByLanguageName(name);
+      res.ok ({
+        message: 'Delete language success.',
+        data: language
+      });
+    }catch (e) {
+      res.serverError(e);
+    }
+  }
+}
